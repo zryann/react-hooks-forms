0646e90c0042e1915587a8a893e07aa4de78e33b
Zryan Muhammed Jaafer <zryan.muhammed.jaafer@gmail.com>
2022-09-20 03:03:17 +0300

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,18 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
